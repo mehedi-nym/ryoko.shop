508d3a854853a049fae4f71fddd57c459ef436d0
mhnayemdb <mhnayem@dbtours.net>
2025-04-26 17:17:17 +0600

    Fixed product add - remove functionality

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,18 @@
 import { v2 as cloudinary } from "cloudinary"
 import productModel from "../models/productModel.js"
 
+
+  
+
 //function for add product
 const addProduct = async (req,res) => {
     try {
 
         const { name, description, price, category,subCategory, sizes, bestseller } = req.body
+
+        if (!name || !description || !price || !category) {
+            return res.status(400).json({ success: false, message: "Missing required fields" });
+          }
 
         const image1 =req.files.image1 && req.files.image1[0]
         const image2 =req.files.image2 && req.files.image2[0]
@@ -28,17 +35,17 @@ const addProduct = async (req,res) => {
             price: Number(price),
             subCategory,
             bestseller: bestseller === "true" ? true : false,
-            sizes: JSON.parse(sizes),
+            sizes: sizes ? JSON.parse(sizes) : [],
             image: imagesUrl,
             date: Date.now()
-        }
+        }        
 
         console.log(productData);
 
         const product = new productModel(productData);
         await product.save()
 
-        res.json({success: true, message:"Product successfully addedd" })
+        res.json({ success: true, message: "Product Added", product });
 
     } catch (error) {
         console.log(error)
@@ -49,12 +56,28 @@ const addProduct = async (req,res) => {
 
 //function for list product
 const listProducts = async (req,res) => {
-    
+    try {
+
+        const products = await productModel.find({});
+        res.json({success:true,products})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message: error.message }) 
+    }
 }
 
 //function for removing product
 const removeProduct = async (req,res) => {
-    
+    try{
+
+        await productModel.findByIdAndDelete(req.body.id)
+        res.json({success:true,message:"Product Removed Successfully"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message: error.message })
+    }
 }
 
 //function for single product info
